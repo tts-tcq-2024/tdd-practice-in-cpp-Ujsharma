e24ea7535a55a80d8ba5238933460bb4481d73fe
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-15 16:38:43 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -4,6 +4,11 @@
 #include <algorithm>
 #include <vector>
 
+int StringCalculator::add(const std::string& input) {
+    if (input.empty()) {
+        return 0;
+    }
+
 bool checkifInput0orEmpty(const std::string& input) {
     return input.empty() || input == "0";
 }

45344a17068b11a5d467da324e7a949b371fca0f
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-15 17:01:35 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -9,19 +9,3 @@ int StringCalculator::add(const std::string& input) {
         return 0;
     }}
 
-std::string delimiter = ",";
-    if (input.substr(0, 2) == "//") {
-        size_t pos = input.find("\n");
-        delimiter = input.substr(2, pos - 2);
-        input = input.substr(pos + 1);
-    }
-
-    // Extracting numbers
-    std::vector<int> numbers = extractNumbers(input, delimiter);
-
-for (int num : numbers) {
-        if (num < 0) {
-            throw std::runtime_error("Negatives not allowed");
-        }
-    }
-

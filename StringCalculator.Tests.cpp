dbf30573b8b88ac8a29075617d307f2014cc79d1
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-15 17:14:10 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -23,23 +23,24 @@ TEST(StringCalculatorAddTests, ExpectExceptionForNegativeNumbers) {
     ASSERT_THROW({
         std::string input = "-1,2";
         StringCalculator objUnderTest;
-       objUnderTest.add(input);
-        }, std::runtime_error);
+        objUnderTest.add(input);
+    }, std::runtime_error);
 }
 
 TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
     int expectedresult = 6;
     std::string input = "1\n2,3";
-     StringCalculator objUnderTest;
-    int result =objUnderTest.add(input);
+    StringCalculator objUnderTest;
+    int result = objUnderTest.add(input);
 
     ASSERT_EQ(result, expectedresult);
+}
 
 TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
     int expectedresult = 1;
     std::string input = "1,1001";
     StringCalculator objUnderTest;
-    int result =objUnderTest.add(input);
+    int result = objUnderTest.add(input);
 
     ASSERT_EQ(result, expectedresult);
 }
@@ -53,6 +54,7 @@ TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
     ASSERT_EQ(result, expectedresult);
 }
 
+// Main function to run all tests
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();

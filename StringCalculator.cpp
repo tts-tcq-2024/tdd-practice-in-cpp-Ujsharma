78328c81288a768184710e90d4909b9a776a0d8e
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-15 16:40:23 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -7,7 +7,7 @@
 int StringCalculator::add(const std::string& input) {
     if (input.empty()) {
         return 0;
-    }
+    }}
 
 bool checkifInput0orEmpty(const std::string& input) {
     return input.empty() || input == "0";

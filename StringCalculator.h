5b561caf77f2c5c7bfa33dd8097c543871a3aaed
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-15 17:09:46 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,13 +1,17 @@
-// StringCalculator.h
-
 #ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
 
 #include <string>
+#include <vector>
+#include <stdexcept>
 
 class StringCalculator {
 public:
     int add(const std::string& input);
+
+private:
+    std::vector<int> extractNumbers(const std::string& input, const std::string& delimiter);
 };
 
 #endif // STRINGCALCULATOR_H
+

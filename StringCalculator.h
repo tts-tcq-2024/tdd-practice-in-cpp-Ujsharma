9036b2f95c810a247a77db6911b7e7d557c7f29b
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-24 11:49:52 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,25 +1,11 @@
 #ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <string>
 
 class StringCalculator {
 public:
     int add(const std::string& numbers);
 };
 
-// Function declarations
-int add_arr(int *arr, int cnt);
-int check_num(const char ch);
-void strtonum(const char *ch, int *num_arr, int *cnt);
-int sum(const char *ch);
-int check_neg(const char *ch);
-int check_empty(const char* ch);
-int check_neg_empty(const char *ch);
-int check_string(const char *ch);
-int add(const char *ch);
-
 #endif // STRINGCALCULATOR_H
-

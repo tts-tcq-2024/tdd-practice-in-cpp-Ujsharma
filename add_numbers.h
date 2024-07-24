117acda5a631af578993e11508d373165b0ae548
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-24 12:03:50 +0530

    Update add_numbers.h

--- add_numbers.h
+++ add_numbers.h
@@ -1,6 +1,17 @@
 #ifndef ADD_NUMBERS_H
 #define ADD_NUMBERS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Function to add numbers from a string
+int add(const char *ch);
+
+#ifdef __cplusplus
+}
+#endif
+
 int add(const char *ch);
 int add_arr(int *arr, int cnt);
 int check_num(const char ch);

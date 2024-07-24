766239532338a7f3301b5625bafece857a1b5acf
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-24 11:36:07 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,17 +1,20 @@
 #ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
 
-#include <string>
-#include <vector>
-#include <stdexcept>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-class StringCalculator {
-public:
-    int add(const std::string& input);
-
-private:
-    std::vector<int> extractNumbers(const std::string& input, const std::string& delimiter);
-};
+// Function declarations
+int add_arr(int *arr, int cnt);
+int check_num(const char ch);
+void strtonum(const char *ch, int *num_arr, int *cnt);
+int sum(const char *ch);
+int check_neg(const char *ch);
+int check_empty(const char* ch);
+int check_neg_empty(const char *ch);
+int check_string(const char *ch);
+int add(const char *ch);
 
 #endif // STRINGCALCULATOR_H
 

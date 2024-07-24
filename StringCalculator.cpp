9cb042db94cc2a256209bc29151f6e9583295a05
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-24 11:44:42 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "StringCalculator.h"
+#include "add_numbers.h"
+
 int StringCalculator::add(const std::string& numbers) {
+    if (numbers.empty()) {
+        return 0;
+    }
     return ::add(numbers.c_str());
 }
+
 
 // Function to add values in an array if they are less than 1001
 int add_arr(int *arr, int cnt)

6a937e88309868138db5c6f2b04ea8e7d5dd658d
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-25 18:52:24 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,18 +1,19 @@
+#ifndef STRINGCALCULATOR_HPP
+#define STRINGCALCULATOR_HPP
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+
 class StringCalculator {
 public:
-    // Method to calculate the sum of numbers from the input string
-    int add(const std::string& input);
+    static int add(const std::string& input);
 
 private:
-    // Function to check if a number is negative and throw an exception if it is
     static void checkForNegative(int number);
-
-    // Function to check if a number is greater than 1000 and exclude it
     static int checkforGreaterthan1000(int number);
-
-    // Function to extract numbers with 1 to 4 digits from a string while preserving the order
     static std::vector<int> extractNumbers(const std::string& input);
-
-    // Function to calculate the sum of numbers in a vector
     static int calculateSum(const std::vector<int>& numbers);
 };
+
+#endif // STRINGCALCULATOR_HPP
